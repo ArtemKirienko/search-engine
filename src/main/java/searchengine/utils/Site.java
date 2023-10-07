a3d593d0cbcbaba969727f2500d3df30541e46d2
Artem Kirienko <kiartemchik1985@gmail.com>
2023-10-07 15:26:50 +0300

    added metod "equals" to "site" class

--- src/main/java/searchengine/utils/Site.java
+++ src/main/java/searchengine/utils/Site.java
@@ -3,11 +3,21 @@ package searchengine.utils;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Setter
 @Getter
 public class Site {
     private String url;
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Site site = (Site) o;
+        return Objects.equals(url, site.url) && Objects.equals(name, site.name);
+    }
 
     @Override
     public int hashCode(){
